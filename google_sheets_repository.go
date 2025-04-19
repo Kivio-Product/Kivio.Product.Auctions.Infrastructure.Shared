08c74f9fe02381f625a4bd22b3a0e681ef6b79a5
David trujillo <davidtrujillo2434@gmail.com>
2025-04-19 16:24:09 -0500

    refactor: add infra integration

--- google_sheets_repository.go
+++ google_sheets_repository.go
@@ -5,53 +5,70 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/Kivio-Product/Kivio.Product.Auctions.Rules/internal/domain"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
 
+type SheetData struct {
+	Values [][]interface{}
+}
+
 type GoogleSheetsRepository interface {
-	GetSheetData() (domain.SheetData, error)
+	ConnectToSheet(spreadsheetID string, readRange string) error
+	GetSheetData() (SheetData, error)
 }
 
 type googleSheetsRepository struct {
 	spreadsheetID string
 	readRange     string
+	service       *sheets.Service
 }
 
 func NewGoogleSheetsRepository() GoogleSheetsRepository {
-	return &googleSheetsRepository{
-		spreadsheetID: "1W--Lxh6vk9Iw0OcdTKMl__KhJdkBn0Sbim38vyQ7co8",
-		readRange:     "Sheet1!A1:V4",
-	}
+	return &googleSheetsRepository{}
 }
 
-func (r *googleSheetsRepository) GetSheetData() (domain.SheetData, error) {
+func (r *googleSheetsRepository) ConnectToSheet(spreadsheetID string, readRange string) error {
 	ctx := context.Background()
 
 	credentialsJSON, err := os.ReadFile("credentials.json")
 	if err != nil {
-		return domain.SheetData{}, err
+		return fmt.Errorf("unable to read client secret file: %v", err)
 	}
 
 	config, err := google.JWTConfigFromJSON(credentialsJSON, sheets.SpreadsheetsReadonlyScope)
 	if err != nil {
-		return domain.SheetData{}, err
+		return fmt.Errorf("unable to parse client secret file to config: %v", err)
 	}
 
 	client := config.Client(ctx)
 	srv, err := sheets.New(client)
 	if err != nil {
-		return domain.SheetData{}, err
+		return fmt.Errorf("unable to create Sheets client: %v", err)
 	}
 
-	resp, err := srv.Spreadsheets.Values.Get(r.spreadsheetID, r.readRange).Context(ctx).Do()
+	// Save connection data
+	r.service = srv
+	r.spreadsheetID = spreadsheetID
+	r.readRange = readRange
 
-	fmt.Printf("Response: %v\n", resp)
+	fmt.Println("Successfully connected to the Google Sheet.")
+	return nil
+}
 
+func (r *googleSheetsRepository) GetSheetData() (SheetData, error) {
+	if r.service == nil || r.spreadsheetID == "" || r.readRange == "" {
+		return SheetData{}, fmt.Errorf("Google Sheets not connected. Please call ConnectToSheet first")
+	}
+
+	ctx := context.Background()
+
+	resp, err := r.service.Spreadsheets.Values.Get(r.spreadsheetID, r.readRange).Context(ctx).Do()
 	if err != nil {
-		return domain.SheetData{}, err
+		return SheetData{}, fmt.Errorf("unable to retrieve data from sheet: %v", err)
 	}
 
-	return domain.SheetData{Values: resp.Values}, nil
+	fmt.Printf("Successfully read data from sheet: %v\n", resp.Values)
+
+	return SheetData{Values: resp.Values}, nil
 }
